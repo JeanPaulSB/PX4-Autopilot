dfb618ae504388c2f4b566b053cbc057b157ca36
JeanPaulSB <jeanpaulsierraboom@gmail.com>
2025-05-23 17:23:07 -0500

    fix: add env variables

--- swarm_scripts/launch.py
+++ swarm_scripts/launch.py
@@ -6,3 +6,14 @@ DRONE_COUNT = 5
 # Starts the PX4 SITL simulation
 subprocess.Popen(["PX4_SYS_AUTOSTART=4001", "PX4_SIM_MODEL=gz_x500", "./build/px4_sitl_default/bin/px4","-i","1"])
 
+# Loop to launch multiple drones
+for i in range(2, DRONE_COUNT + 1):
+	env = {
+		'PX4_SYS_AUTOSTART': '4001',
+		'PX4_SIM_MODEL': 'gz_x500'
+	}
+	subprocess.Popen(
+		["./build/px4_sitl_default/bin/px4", "-i", str(i)],
+		env=env,
+		cwd="/Users/jeansierraboom/Downloads/PX4-Autopilot"
+	)

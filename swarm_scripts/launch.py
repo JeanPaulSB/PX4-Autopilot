12fd2bde3deb36ae63d98d924ffe8d305765242c
JeanPaulSB <jeanpaulsierraboom@gmail.com>
2025-05-23 17:46:45 -0500

    feat: add core loop

--- swarm_scripts/launch.py
+++ swarm_scripts/launch.py
@@ -3,10 +3,19 @@ import subprocess
 import os
 from jinja2 import Template
 
+DRONE_COUNT = 6
+
 # Template string to launch the vehicle
-template_str = """PX4_SYS_AUTOSTART={{ autostart }} PX4_GZ_MODEL={{ modelo }} ./build/px4_sitl_default/bin/px4 -i {{ instancia }}"""
+template_str = """PX4_SYS_AUTOSTART={{ autostart }} PX4_GZ_MODEL={{ model }} ./build/px4_sitl_default/bin/px4 -i {{ instance }}"""
 template = Template(template_str)
 template_result = template.render({"autostart": 4001, "modelo": "x500", "instancia": 1})
 
-subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', template_result])
+for index,_ in enumerate(range(DRONE_COUNT)):
+    args = {
+        "autostart": 4001,
+	"model": "x500",
+	"instance": index
+    }
+    template_result = template.render(**args)
+    subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', template_result])
 
